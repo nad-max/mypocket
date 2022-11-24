116ad7da968f148482079fc556b24b1505ae8206
user <nada>
2022-11-24 21:04:46 +0100

    first commit

---
+++ src/ViewController/HomeViewController.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ViewController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class HomeViewController  {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    
+}
